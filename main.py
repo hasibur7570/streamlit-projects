11d4f916b39783cfc1cc0cc7e14a0ec3ba3e9176
hasibur7570 <hasibur.rahman7570@gmail.com>
2026-06-03 11:14:35 +0600

    Streamlit display data: text, write, magic method

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello from streamlit!")
+st.title("Hello from Data Science Academy!")
+st.subheader("Brewed with Data")
+st.text("Weclocme to your first interactive lms dashboard!")
+st.write("Choose your favourite domain")
+
+domain = st.selectbox("Your favourite domain: ", ["Data Science", "Machine Learning", "AI Generalist", "AI Builders"])
+st.write(f"You choose {domain}. Excellent choice!")
+st.success("Your domain is selectedt. Now you're ready to learn.")
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': ["Rakesh", "Rafi", "Mohit", "Sujata"]
+}))
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+df
+
